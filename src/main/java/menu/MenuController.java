45533aa8fe7514c9c7c6604ce856d24ec027590b
VasylNyzhnyk <VasylNyzhnyk@users.noreply.github.com>
2019-03-17 17:13:06 +0200

    rename menu

--- src/main/java/menu/MenuController.java
+++ src/main/java/menu/MenuController.java
@@ -8,20 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MenuController {
-
-
-
     @GetMapping("/vegan")
     public String vegan(@RequestParam(name="name", required=false, defaultValue="vegan") String name, Model model) {
         model.addAttribute("name", name);
-
-
         return "vegan";
-
-
     }
 
-   @GetMapping("/noVegan")
+    @GetMapping("/noVegan")
     public String noVegan(@RequestParam(name="name", required=false, defaultValue="noVegan") String name, Model model) {
         model.addAttribute("name", name);
         return "noVegan";
