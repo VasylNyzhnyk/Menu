0e1488a209ed2f40f7bdb7c71b3bf1d85625e252
VasylNyzhnyk <VasylNyzhnyk@users.noreply.github.com>
2019-03-17 13:54:41 +0200

    rename menu

--- src/main/java/menu/MenuController.java
+++ src/main/java/menu/MenuController.java
@@ -1,18 +1,14 @@
-package hello;
+package menu;
 
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class MenuController {
-    
+
 
 
     @GetMapping("/vegan")
